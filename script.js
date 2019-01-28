6be40163dfab0635419c786065e9368c0e667726
Chaim Bucsko <cbucsko@aish.com>
2019-01-28 21:08:45 +0200

    Tweaked scrollIntoView

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function handleClickLink(event) {
     const block = event.target.dataset.block;
     console.log(block);
-    document.getElementById(block).scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(block).scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+    });
 }
 
 document.querySelectorAll('.link').forEach(el => {
