4702a40bfc05108037baa33a5fec4edbc118b358
Chaim Bucsko <cgbucsko@gmail.com>
2019-02-11 20:49:59 +0200

    Added logo

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function handleClickLink(event) {
     const block = event.target.dataset.block;
-    console.log(block);
     document.getElementById(block).scrollIntoView({ 
         behavior: 'smooth',
         block: 'start'
