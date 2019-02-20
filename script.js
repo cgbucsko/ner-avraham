b6d240e46a4125fb525b7fe7d7b967c4de64fb4a
Chaim Bucsko <cgbucsko@gmail.com>
2019-02-20 19:17:21 +0200

    Set up donate buttons with donorbox

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ let waypoint = new Waypoint({
     },
     offset: 65
 });
+
+// STRIPE PAYMENT
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")("sk_test_SyGiw3HxW5ZdIxJDUddxKybu");
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+const token = request.body.stripeToken; // Using Express
+
+(async () => {
+  const charge = await stripe.charges.create({
+    amount: 999,
+    currency: 'usd',
+    description: 'Example charge',
+    source: token,
+  });
+})();
